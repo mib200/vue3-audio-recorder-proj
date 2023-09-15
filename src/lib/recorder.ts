3a8897f3f2a8e3145f495dbd6fcc908d9ecde652
Manish Kumar <mib200@gmail.com>
2023-09-15 11:34:15 +0530

    ts ignore

--- src/lib/recorder.ts
+++ src/lib/recorder.ts
@@ -4,28 +4,44 @@ import { convertTimeMMSS } from './utils'
 
 export default class {
   constructor(options = {}) {
+    // @ts-ignore
     this.beforeRecording = options.beforeRecording
+    // @ts-ignore
     this.pauseRecording = options.pauseRecording
+    // @ts-ignore
     this.afterRecording = options.afterRecording
+    // @ts-ignore
     this.micFailed = options.micFailed
+    // @ts-ignore
     this.format = options.format
 
+    // @ts-ignore
     this.encoderOptions = {
+      // @ts-ignore
       bitRate: options.bitRate,
+      // @ts-ignore
       sampleRate: options.sampleRate
     }
 
+    // @ts-ignore
     this.bufferSize = 4096
+    // @ts-ignore
     this.records = []
 
+    // @ts-ignore
     this.isPause = false
+    // @ts-ignore
     this.isRecording = false
 
+    // @ts-ignore
     this.duration = 0
+    // @ts-ignore
     this.volume = 0
 
+    // @ts-ignore
     this.wavSamples = []
 
+    // @ts-ignore
     this._duration = 0
 
     console.log(this)
@@ -40,6 +56,7 @@ export default class {
       }
     }
 
+    // @ts-ignore
     this.beforeRecording && this.beforeRecording('start recording')
 
     navigator.mediaDevices
@@ -47,80 +64,117 @@ export default class {
       .then(this._micCaptured.bind(this))
       .catch(this._micError.bind(this))
 
+    // @ts-ignore
     this.isPause = false
+    // @ts-ignore
     this.isRecording = true
 
+    // @ts-ignore
     if (this._isMp3() && !this.lameEncoder) {
+      // @ts-ignore
       console.log(this.encoderOptions)
+      // @ts-ignore
       this.lameEncoder = new Mp3Encoder(this.encoderOptions)
     }
   }
 
   stop() {
+    // @ts-ignore
     this.stream.getTracks().forEach((track) => track.stop())
+    // @ts-ignore
     this.input.disconnect()
+    // @ts-ignore
     this.processor.disconnect()
+    // @ts-ignore
     this.context.close()
 
     let record = null
 
     if (this._isMp3()) {
+      // @ts-ignore
       record = this.lameEncoder.finish()
     } else {
-      let wavEncoder = new WavEncoder({
+      const wavEncoder = new WavEncoder({
+        // @ts-ignore
         bufferSize: this.bufferSize,
+        // @ts-ignore
         sampleRate: this.encoderOptions.sampleRate,
+        // @ts-ignore
         samples: this.wavSamples
       })
       record = wavEncoder.finish()
+      // @ts-ignore
       this.wavSamples = []
     }
 
+    // @ts-ignore
     record.duration = convertTimeMMSS(this.duration)
+    // @ts-ignore
     this.records.push(record)
 
+    // @ts-ignore
     this._duration = 0
+    // @ts-ignore
     this.duration = 0
 
+    // @ts-ignore
     this.isPause = false
+    // @ts-ignore
     this.isRecording = false
 
+    // @ts-ignore
     this.afterRecording && this.afterRecording(record)
   }
 
   pause() {
+    // @ts-ignore
     this.stream.getTracks().forEach((track) => track.stop())
+    // @ts-ignore
     this.input.disconnect()
+    // @ts-ignore
     this.processor.disconnect()
 
+    // @ts-ignore
     this._duration = this.duration
+    // @ts-ignore
     this.isPause = true
 
+    // @ts-ignore
     this.pauseRecording && this.pauseRecording('pause recording')
   }
 
   recordList() {
+    // @ts-ignore
     return this.records
   }
 
   lastRecord() {
+    // @ts-ignore
     return this.records.slice(-1).pop()
   }
 
-  _micCaptured(stream) {
+  _micCaptured(stream: any) {
+    // @ts-ignore
     this.context = new (window.AudioContext || window.webkitAudioContext)()
+      // @ts-ignore
     this.duration = this._duration
+      // @ts-ignore
     this.input = this.context.createMediaStreamSource(stream)
+      // @ts-ignore
     this.processor = this.context.createScriptProcessor(this.bufferSize, 1, 1)
+      // @ts-ignore
     this.stream = stream
 
+      // @ts-ignore
     this.processor.onaudioprocess = (ev) => {
       const sample = ev.inputBuffer.getChannelData(0)
       let sum = 0.0
 
       if (this._isMp3()) {
+        // @ts-ignore
         this.lameEncoder.encode(sample)
       } else {
+        // @ts-ignore
         this.wavSamples.push(new Float32Array(sample))
       }
 
@@ -128,19 +182,25 @@ export default class {
         sum += sample[i] * sample[i]
       }
 
+      // @ts-ignore
       this.duration = parseFloat(this._duration) + parseFloat(this.context.currentTime.toFixed(2))
+      // @ts-ignore
       this.volume = Math.sqrt(sum / sample.length).toFixed(2)
     }
 
+    // @ts-ignore
     this.input.connect(this.processor)
+    // @ts-ignore
     this.processor.connect(this.context.destination)
   }
 
-  _micError(error) {
+  _micError(error: any) {
+    // @ts-ignore
     this.micFailed && this.micFailed(error)
   }
 
   _isMp3() {
+    // @ts-ignore
     return (this.format || '').toLowerCase() === 'mp3'
   }
 }
--- src/lib/wav-encoder.ts
+++ src/lib/wav-encoder.ts
@@ -1,30 +1,38 @@
 export default class {
-  constructor(options) {
+  constructor(options: any) {
+    // @ts-ignore
     this.bufferSize = options.bufferSize || 4096
+    // @ts-ignore
     this.sampleRate = options.sampleRate
+    // @ts-ignore
     this.samples = options.samples
   }
 
   finish() {
     this._joinSamples()
-
-    let buffer = new ArrayBuffer(44 + this.samples.length * 2)
-    let view = new DataView(buffer)
+    // @ts-ignore
+    const buffer = new ArrayBuffer(44 + this.samples.length * 2)
+    const view = new DataView(buffer)
 
     this._writeString(view, 0, 'RIFF') // RIFF identifier
+    // @ts-ignore
     view.setUint32(4, 36 + this.samples.length * 2, true) // RIFF chunk length
     this._writeString(view, 8, 'WAVE') // RIFF type
     this._writeString(view, 12, 'fmt ') // format chunk identifier
     view.setUint32(16, 16, true) // format chunk length
     view.setUint16(20, 1, true) // sample format (raw)
     view.setUint16(22, 1, true) // channel count
+    // @ts-ignore
     view.setUint32(24, this.sampleRate, true) // sample rate
+    // @ts-ignore
     view.setUint32(28, this.sampleRate * 4, true) // byte rate (sample rate * block align)
     view.setUint16(32, 4, true) // block align (channel count * bytes per sample)
     view.setUint16(34, 16, true) // bits per sample
     this._writeString(view, 36, 'data') // data chunk identifier
+    // @ts-ignore
     view.setUint32(40, this.samples.length * 2, true) // data chunk length
 
+    // @ts-ignore
     this._floatTo16BitPCM(view, 44, this.samples)
 
     const blob = new Blob([view], { type: 'audio/wav' })
@@ -36,27 +44,33 @@ export default class {
     }
   }
 
+  // @ts-ignore
   _floatTo16BitPCM(output, offset, input) {
     for (let i = 0; i < input.length; i++, offset += 2) {
-      let s = Math.max(-1, Math.min(1, input[i]))
+      const s = Math.max(-1, Math.min(1, input[i]))
       output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true)
     }
   }
 
   _joinSamples() {
-    let recordLength = this.samples.length * this.bufferSize
-    let joinedSamples = new Float64Array(recordLength)
+    // @ts-ignore
+    const recordLength = this.samples.length * this.bufferSize
+    const joinedSamples = new Float64Array(recordLength)
     let offset = 0
 
+    // @ts-ignore
     for (let i = 0; i < this.samples.length; i++) {
-      let sample = this.samples[i]
+      // @ts-ignore
+      const sample = this.samples[i]
       joinedSamples.set(sample, offset)
       offset += sample.length
     }
 
+    // @ts-ignore
     this.samples = joinedSamples
   }
 
+  // @ts-ignore
   _writeString(view, offset, string) {
     for (let i = 0; i < string.length; i++) {
       view.setUint8(offset + i, string.charCodeAt(i))
